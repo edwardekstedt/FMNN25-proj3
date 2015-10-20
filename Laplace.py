017f67583f3fdebb70d707a0ce520f198b627c71
Johannes Olsson <joholss@gmail.com>
2015-10-20 13:43:03 +0200

    Code cleanup
    Cleaned up the code in laplace.py

--- Laplace.py
+++ Laplace.py
@@ -24,9 +24,10 @@ class laplaceSolver(object):
         #N = AMOUNT OF ROWS
         #M = AMOUNT OF COLUMNS
         self.N = self.N-2
-        self.M = self.M-2
-        if conditionType == 'Neumann':
-            self.M = self.M+1
+        if conditionType == 'Dirichlet':
+            self.M = self.M-2
+        elif conditionType == 'Neumann':
+            self.M = self.M-1
         self._setRhs(conditionType,dir)
         return self._update(conditionType,dir)
 
@@ -51,31 +52,24 @@ class laplaceSolver(object):
             self.rhs[0,:] = -self.bc[0,1:]/self.dx**2
             self.rhs[-1,:] = -self.bc[-1,1:]/self.dx**2
             self.rhs[:,0] = self.rhs[:,0]- self.bc[1:-1,0]/self.dx**2
-            self.rhs[:,-1] = self.rhs[:,-1] - self.bc[1:-1,-1]/self.dx ## Change to Neumann
+            self.rhs[:,-1] = self.rhs[:,-1] - self.bc[1:-1,-1]/self.dx
         else:
             raise NameError('Condition must be either "Dirichlet" or "Neumann".')
+    
     def _update(self,conditionType,direction):
+        self.rhs = self.rhs.reshape(self.M*self.N)
+        sub = ones([1,self.M-1])           
         if conditionType == 'Dirichlet':
-            self.rhs = self.rhs.reshape(self.M*self.N)
-            sub = ones([1,self.M-1])        
             main = (-4*ones([1,self.M]))
-            D = diagflat(main) + diagflat(sub,1) + diagflat(sub,-1)
-            BL = kron(eye(self.N),D)
-            T = BL+diagflat(ones([1,self.M*(self.N-1)]),-self.M)+diagflat(ones([1,self.M*(self.N-1)]),self.M)
-            T = T*1/self.dx**2  
-            U = linalg.solve(T,self.rhs)
-            U = U.reshape(self.N,self.M)
-            self.bc[1:-1,1:-1] = U
-        if conditionType == 'Neumann':
-            self.rhs = self.rhs.reshape(self.M*self.N)
-            sub = ones([1,self.M-1])
+        elif conditionType == 'Neumann':
             main = append([-4*ones([1,self.M-1])],[-3])
-            D = diagflat(main) + diagflat(sub,1) + diagflat(sub,-1)
-            BL = kron(eye(self.N), D)
-            T = BL+diagflat(ones([1,self.M*(self.N-1)]),-self.M)+diagflat(ones([1,self.M*(self.N-1)]),self.M)
-            T = T*1/self.dx**2
-            U = linalg.solve(T,self.rhs)
-            U = U.reshape(self.N,self.M)
+        T = diagflat(main) + diagflat(sub,1) + diagflat(sub,-1)
+        A = (kron(eye(self.N),T)+diagflat(ones([1,self.M*(self.N-1)]),-self.M)+diagflat(ones([1,self.M*(self.N-1)]),self.M))/self.dx**2
+        U = linalg.solve(A,self.rhs)
+        U = U.reshape(self.N,self.M)
+        if conditionType == 'Dirichlet':
+            self.bc[1:-1,1:-1] = U
+        elif conditionType == 'Neumann':
             self.bc[1:-1,1:] = U
             if direction == 'east':
                 pass
@@ -86,8 +80,3 @@ class laplaceSolver(object):
             elif direction =='south':
                 self.bc = rot90(self.bc,1)
         return self.bc
- 
-#M = array([[40.,40,40,40],[15,0,0,15],[15,0,0,15],[15,0,0,15],[15,0,0,15],[15,0,0,15],[5,5,5,5]])
-#solved = laplaceSolver(1/3.)
-#U = solved(M, 'Neumann', 'east')
-#print(U)
