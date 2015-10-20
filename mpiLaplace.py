c12644e4c99fd92efe147909207b1e4d56724f8c
Johannes Olsson <joholss@gmail.com>
2015-10-20 13:51:54 +0200

    Implemented Relaxation

--- mpiLaplace.py
+++ mpiLaplace.py
@@ -13,6 +13,10 @@ rank = comm.Get_rank()
 
 n = int(sys.argv[1])
 dx = float(sys.argv[2])
+if float(sys.argv[3]):
+    omega = float(sys.argv[3])
+else:
+    omega = 0.8
 bcW = numpy.zeros([int(1/dx-1),1])
 bcE = numpy.zeros([int(1/dx-1),1])
 neumannW = numpy.zeros([int(1/dx-1),1])
@@ -27,9 +31,10 @@ for i in range(n):
     if rank == 2: ## Small Room East
         comm.Recv(neumannE,source=0)
         #print 'Eastern Neumann recieved'
-        SmallE = room.getRoom('SmallE')
+        OldSmallE = room.getRoom('SmallE')
+        SmallE = OldSmallE.copy()        
         SmallE[1:-1,0] = neumannE.T
-        SmallE = room(SmallE)
+        SmallE = (1-omega)*OldSmallE + omega*room(SmallE)
         bcE = SmallE[1:-1,0]
         if i==n-1:
             comm.Send(numpy.ascontiguousarray(SmallE),dest=0)
@@ -40,9 +45,10 @@ for i in range(n):
     if rank == 1: ## Small Room West
         comm.Recv(neumannW,source=0)
         #print 'Western Neumann recieved'
-        SmallW = room.getRoom('SmallW')
+        OldSmallW = room.getRoom('SmallW')
+        SmallW = OldSmallW.copy()        
         SmallW[1:-1,-1]= neumannW.T
-        SmallW = room(SmallW)
+        SmallW = (1-omega)*OldSmallW + omega*room(SmallW)
         bcW = SmallW[1:-1,-1]
         if i ==n-1:
             comm.Send(numpy.ascontiguousarray(SmallW),dest=0)
@@ -58,9 +64,10 @@ for i in range(n):
            # print 'bcW recieved in iteration',i+1
             comm.Recv(bcE,source =2)
           #  print 'bcE recieved in iteration',i+1
+            OldLarge = Large.copy()            
             Large[N/2+1:-1,0] = bcW.T
             Large[1:N/2,-1] = bcE.T
-            Large = room(Large)
+            Large = (1-omega)*OldLarge + omega*room(Large)
         neumannW = -(Large[N/2+1:-1,0]-Large[N/2+1:-1,1])/dx
         neumannE = -(Large[1:N/2,-1]-Large[1:N/2,-2])/dx
         comm.Send(neumannW,dest=1)
